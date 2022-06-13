59f6bdda61a897d249981f1127c62257c71c183a
Renad <renad.gharz@outlook.com>
2022-06-13 14:34:58 -0600

    reading .dbf files

---
+++ data_imports.R
@@ -0,0 +1,38 @@
+library(foreign)
+
+# Fixed 2019
+fixed_2019_q1 <- read.dbf('./raw/2019-q1/gps_fixed_tiles.dbf', as.is=TRUE)
+fixed_2019_q2 <- read.dbf('./raw/2019-q2/gps_fixed_tiles.dbf', as.is=TRUE)
+fixed_2019_q3 <- read.dbf('./raw/2019-q3/gps_fixed_tiles.dbf', as.is=TRUE)
+fixed_2019_q4 <- read.dbf('./raw/2019-q4/gps_fixed_tiles.dbf', as.is=TRUE)
+
+# Mobile 2019
+mobile_2019_q1 <- read.dbf('./raw/2019-q1/gps_mobile_tiles.dbf', as.is=TRUE)
+mobile_2019_q2 <- read.dbf('./raw/2019-q2/gps_mobile_tiles.dbf', as.is=TRUE)
+mobile_2019_q3 <- read.dbf('./raw/2019-q3/gps_mobile_tiles.dbf', as.is=TRUE)
+mobile_2019_q4 <- read.dbf('./raw/2019-q4/gps_mobile_tiles.dbf', as.is=TRUE)
+
+# Fixed 2020
+fixed_2020_q1 <- read.dbf('./raw/2020-q1/gps_fixed_tiles.dbf', as.is=TRUE)
+fixed_2020_q2 <- read.dbf('./raw/2020-q2/gps_fixed_tiles.dbf', as.is=TRUE)
+fixed_2020_q3 <- read.dbf('./raw/2020-q3/gps_fixed_tiles.dbf', as.is=TRUE)
+fixed_2020_q4 <- read.dbf('./raw/2020-q4/gps_fixed_tiles.dbf', as.is=TRUE)
+
+# Mobile 2020
+mobile_2020_q1 <- read.dbf('./raw/2020-q1/gps_mobile_tiles.dbf', as.is=TRUE)
+mobile_2020_q2 <- read.dbf('./raw/2020-q2/gps_mobile_tiles.dbf', as.is=TRUE)
+mobile_2020_q3 <- read.dbf('./raw/2020-q3/gps_mobile_tiles.dbf', as.is=TRUE)
+mobile_2020_q4 <- read.dbf('./raw/2020-q4/gps_mobile_tiles.dbf', as.is=TRUE)
+
+# Fixed 2021
+fixed_2021_q1 <- read.dbf('./raw/2021-q1/gps_fixed_tiles.dbf', as.is=TRUE)
+fixed_2021_q2 <- read.dbf('./raw/2021-q2/gps_fixed_tiles.dbf', as.is=TRUE)
+fixed_2021_q3 <- read.dbf('./raw/2021-q3/gps_fixed_tiles.dbf', as.is=TRUE)
+fixed_2021_q4 <- read.dbf('./raw/2021-q4/gps_fixed_tiles.dbf', as.is=TRUE)
+
+# Mobile 2021
+mobile_2021_q1 <- read.dbf('./raw/2021-q1/gps_mobile_tiles.dbf', as.is=TRUE)
+mobile_2021_q2 <- read.dbf('./raw/2021-q2/gps_mobile_tiles.dbf', as.is=TRUE)
+mobile_2021_q3 <- read.dbf('./raw/2021-q3/gps_mobile_tiles.dbf', as.is=TRUE)
+mobile_2021_q4 <- read.dbf('./raw/2021-q4/gps_mobile_tiles.dbf', as.is=TRUE)
+
