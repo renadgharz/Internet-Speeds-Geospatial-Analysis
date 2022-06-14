b5ec2dc9764bbd4b1cf627f13028f69fa6199659
Renad <renad.gharz@outlook.com>
2022-06-13 19:02:46 -0600

    switched to parquet files

--- data_imports.R
+++ data_imports.R
@@ -1,73 +1,103 @@
-library(foreign)
+library(arrow)
 
 # Fixed 2019
-fixed_2019_q1 <- read.dbf('./raw/2019-q1/gps_fixed_tiles.dbf', as.is=TRUE)
-fixed_2019_q2 <- read.dbf('./raw/2019-q2/gps_fixed_tiles.dbf', as.is=TRUE)
-fixed_2019_q3 <- read.dbf('./raw/2019-q3/gps_fixed_tiles.dbf', as.is=TRUE)
-fixed_2019_q4 <- read.dbf('./raw/2019-q4/gps_fixed_tiles.dbf', as.is=TRUE)
+fixed_2019_q1 <- 
+  read_parquet('./raw/2019-q1/2019-01-01_performance_fixed_tiles.parquet', 
+               as.is=TRUE)
 
-# List of dataframes for fixed data in 2019
-fixed_2019 <- list(fixed_2019_q1, 
-                   fixed_2019_q2, 
-                   fixed_2019_q3, 
-                   fixed_2019_q4)
+fixed_2019_q2 <- 
+  read_parquet('./raw/2019-q2/2019-04-01_performance_fixed_tiles.parquet', 
+               as.is=TRUE)
+
+fixed_2019_q3 <- 
+  read_parquet('./raw/2019-q3/2019-07-01_performance_fixed_tiles.parquet', 
+               as.is=TRUE)
+
+fixed_2019_q4 <- 
+  read_parquet('./raw/2019-q4/2019-10-01_performance_fixed_tiles.parquet', 
+               as.is=TRUE)
 
 # Mobile 2019
-mobile_2019_q1 <- read.dbf('./raw/2019-q1/gps_mobile_tiles.dbf', as.is=TRUE)
-mobile_2019_q2 <- read.dbf('./raw/2019-q2/gps_mobile_tiles.dbf', as.is=TRUE)
-mobile_2019_q3 <- read.dbf('./raw/2019-q3/gps_mobile_tiles.dbf', as.is=TRUE)
-mobile_2019_q4 <- read.dbf('./raw/2019-q4/gps_mobile_tiles.dbf', as.is=TRUE)
+mobile_2019_q1 <- 
+  read_parquet('./raw/2019-q1/2019-01-01_performance_mobile_tiles.parquet', 
+               as.is=TRUE)
 
-# List of dataframes for mobile data in 2019
-mobile_2019 <- list(mobile_2019_q1,
-                    mobile_2019_q2,
-                    mobile_2019_q3,
-                    mobile_2019_q4)
+mobile_2019_q2 <- 
+  read_parquet('./raw/2019-q2/2019-04-01_performance_mobile_tiles.parquet', 
+               as.is=TRUE)
+
+mobile_2019_q3 <- 
+  read_parquet('./raw/2019-q3/2019-07-01_performance_mobile_tiles.parquet', 
+               as.is=TRUE)
+
+mobile_2019_q4 <- 
+  read_parquet('./raw/2019-q4/2019-10-01_performance_mobile_tiles.parquet', 
+               as.is=TRUE)
 
 # Fixed 2020
-fixed_2020_q1 <- read.dbf('./raw/2020-q1/gps_fixed_tiles.dbf', as.is=TRUE)
-fixed_2020_q2 <- read.dbf('./raw/2020-q2/gps_fixed_tiles.dbf', as.is=TRUE)
-fixed_2020_q3 <- read.dbf('./raw/2020-q3/gps_fixed_tiles.dbf', as.is=TRUE)
-fixed_2020_q4 <- read.dbf('./raw/2020-q4/gps_fixed_tiles.dbf', as.is=TRUE)
+fixed_2020_q1 <- 
+  read_parquet('./raw/2020-q1/2020-01-01_performance_fixed_tiles.parquet', 
+               as.is=TRUE)
 
-# List of dataframes for fixed data in 2020
-fixed_2020 <- list(fixed_2020_q1, 
-                   fixed_2020_q2, 
-                   fixed_2020_q3, 
-                   fixed_2020_q4) 
+fixed_2020_q2 <- 
+  read_parquet('./raw/2020-q2/2020-04-01_performance_fixed_tiles.parquet', 
+               as.is=TRUE)
+
+fixed_2020_q3 <- 
+  read_parquet('./raw/2020-q3/2020-07-01_performance_fixed_tiles.parquet', 
+               as.is=TRUE)
+
+fixed_2020_q4 <- 
+  read_parquet('./raw/2020-q4/2020-10-01_performance_fixed_tiles.parquet', 
+               as.is=TRUE)
 
 # Mobile 2020
-mobile_2020_q1 <- read.dbf('./raw/2020-q1/gps_mobile_tiles.dbf', as.is=TRUE)
-mobile_2020_q2 <- read.dbf('./raw/2020-q2/gps_mobile_tiles.dbf', as.is=TRUE)
-mobile_2020_q3 <- read.dbf('./raw/2020-q3/gps_mobile_tiles.dbf', as.is=TRUE)
-mobile_2020_q4 <- read.dbf('./raw/2020-q4/gps_mobile_tiles.dbf', as.is=TRUE)
+mobile_2020_q1 <- 
+  read_parquet('./raw/2020-q1/2020-01-01_performance_mobile_tiles.parquet', 
+               as.is=TRUE)
 
-# List of dataframes for mobile data in 2020
-mobile_2020 <- list(mobile_2020_q1,
-                    mobile_2020_q2,
-                    mobile_2020_q3,
-                    mobile_2020_q4)
+mobile_2020_q2 <- 
+  read_parquet('./raw/2020-q2/2020-04-01_performance_mobile_tiles.parquet', 
+               as.is=TRUE)
+
+mobile_2020_q3 <- 
+  read_parquet('./raw/2020-q3/2020-07-01_performance_mobile_tiles.parquet', 
+               as.is=TRUE)
+
+mobile_2020_q4 <- 
+  read_parquet('./raw/2020-q4/2020-10-01_performance_mobile_tiles.parquet', 
+               as.is=TRUE)
 
 # Fixed 2021
-fixed_2021_q1 <- read.dbf('./raw/2021-q1/gps_fixed_tiles.dbf', as.is=TRUE)
-fixed_2021_q2 <- read.dbf('./raw/2021-q2/gps_fixed_tiles.dbf', as.is=TRUE)
-fixed_2021_q3 <- read.dbf('./raw/2021-q3/gps_fixed_tiles.dbf', as.is=TRUE)
-fixed_2021_q4 <- read.dbf('./raw/2021-q4/gps_fixed_tiles.dbf', as.is=TRUE)
+fixed_2021_q1 <- 
+  read_parquet('./raw/2021-q1/2021-01-01_performance_fixed_tiles.parquet', 
+               as.is=TRUE)
 
-# List of dataframes for fixed data in 2021
-fixed_2021 <- list(fixed_2021_q1, 
-                   fixed_2021_q2, 
-                   fixed_2021_q3, 
-                   fixed_2021_q4) 
+fixed_2021_q2 <- 
+  read_parquet('./raw/2021-q2/2021-04-01_performance_fixed_tiles.parquet', 
+               as.is=TRUE)
+
+fixed_2021_q3 <- 
+  read_parquet('./raw/2021-q3/2021-07-01_performance_fixed_tiles.parquet', 
+               as.is=TRUE)
+
+fixed_2021_q4 <- 
+  read_parquet('./raw/2021-q4/2021-10-01_performance_fixed_tiles.parquet', 
+               as.is=TRUE)
 
 # Mobile 2021
-mobile_2021_q1 <- read.dbf('./raw/2021-q1/gps_mobile_tiles.dbf', as.is=TRUE)
-mobile_2021_q2 <- read.dbf('./raw/2021-q2/gps_mobile_tiles.dbf', as.is=TRUE)
-mobile_2021_q3 <- read.dbf('./raw/2021-q3/gps_mobile_tiles.dbf', as.is=TRUE)
-mobile_2021_q4 <- read.dbf('./raw/2021-q4/gps_mobile_tiles.dbf', as.is=TRUE)
+mobile_2021_q1 <- 
+  read_parquet('./raw/2021-q1/2021-01-01_performance_mobile_tiles.parquet', 
+               as.is=TRUE)
 
-# List of dataframes for mobile data in 2021
-mobile_2021 <- list(mobile_2021_q1,
-                    mobile_2021_q2,
-                    mobile_2021_q3,
-                    mobile_2021_q4)
+mobile_2021_q2 <- 
+  read_parquet('./raw/2021-q2/2021-04-01_performance_mobile_tiles.parquet', 
+               as.is=TRUE)
+
+mobile_2021_q3 <- 
+  read_parquet('./raw/2021-q3/2021-07-01_performance_mobile_tiles.parquet', 
+               as.is=TRUE)
+
+mobile_2021_q4 <- 
+  read_parquet('./raw/2021-q4/2021-10-01_performance_mobile_tiles.parquet', 
+               as.is=TRUE)
---
+++ dataframe_lists.R
@@ -0,0 +1,35 @@
+# List of dataframes for fixed data in 2019
+fixed_2019 <- list(fixed_2019_q1, 
+                   fixed_2019_q2, 
+                   fixed_2019_q3, 
+                   fixed_2019_q4)
+
+# List of dataframes for mobile data in 2019
+mobile_2019 <- list(mobile_2019_q1,
+                    mobile_2019_q2,
+                    mobile_2019_q3,
+                    mobile_2019_q4)
+
+# List of dataframes for fixed data in 2020
+fixed_2020 <- list(fixed_2020_q1, 
+                   fixed_2020_q2, 
+                   fixed_2020_q3, 
+                   fixed_2020_q4)
+
+# List of dataframes for mobile data in 2020
+mobile_2020 <- list(mobile_2020_q1,
+                    mobile_2020_q2,
+                    mobile_2020_q3,
+                    mobile_2020_q4)
+
+# List of dataframes for fixed data in 2021
+fixed_2021 <- list(fixed_2021_q1, 
+                   fixed_2021_q2, 
+                   fixed_2021_q3, 
+                   fixed_2021_q4)
+
+# List of dataframes for mobile data in 2021
+mobile_2021 <- list(mobile_2021_q1,
+                    mobile_2021_q2,
+                    mobile_2021_q3,
+                    mobile_2021_q4)
